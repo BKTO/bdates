ecea8eeedc0ade351ea4b0fc0729fa2fe16d6acf
BKTO <hello@betterknowtheopposition.com>
2015-10-06 03:23:53 +0000

    more

--- anonymize.sh
+++ anonymize.sh
@@ -8,3 +8,5 @@ export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 export GIT_AUTHOR_NAME="$CORRECT_NAME"
 export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 ' --tag-name-filter cat -- --branches --tags
+
+git push --force --tags origin 'refs/heads/*'
